a15baed763838b36731b999f2a1f06c5bc635221
Oliver Koenigsberger <oliver.koenigsberger@bmw.de>
2024-11-25 15:21:26 +0100

    Adjust config

--- main/test/test_flaky.py
+++ main/test/test_flaky.py
@@ -5,7 +5,7 @@ import unittest
 class TestFlaky(unittest.TestCase):
 
     def test_flaky(self):
-        self.assertTrue(random.random() < 0.3)
+        self.assertTrue(random.random() < 0.20)
 
 if __name__ == "__main__":
     unittest.main()
